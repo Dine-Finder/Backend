2151d06cd02f43d755d402a5c09cf1eb36ecc85c
musaddique333 <mohammedmusaddiquemogal@gmail.com>
2024-07-15 22:27:11 +0100

    created react build and deployed

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-
+from flask_restful import Api
 
 from config import config_dict
 import os
@@ -15,32 +15,33 @@ login_manager = LoginManager()
 jwt = JWTManager()
 
 def createApp():
-    app = Flask(__name__, static_folder="../build", static_url_path="/")
-    CORS(app)
+    app = Flask(__name__, static_folder='build', static_url_path='/')
+    api = Api(app)
+    CORS(app, origins="*")
     
     config_mode = os.getenv('FLASK_ENVIRONMENT', 'Development')
     app.config.from_object(config_dict[config_mode])
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
     db.init_app(app)
-
     bcrypt.init_app(app)
-
     login_manager.init_app(app)
     login_manager.login_view = 'login_form'
     login_manager.login_message_category = 'info'
-
     jwt.init_app(app)
 
     from . import models
-    from . import forms
-    @app.route('/')
-    def index():
-        return app.send_static_file("index.html")
-
     from .routes import routes
-
     app.register_blueprint(routes, url_prefix='/')
+
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def catch_all(path):
+        if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+            return send_from_directory(app.static_folder, path)
+        else:
+            return send_from_directory(app.static_folder, 'index.html')
+
     createDatabase(app)
 
     return app
--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,12 +1,10 @@
 import re
 import json
 import os
-from flask import Blueprint, flash, jsonify, redirect, render_template, request, url_for
-from flask_cors import cross_origin
+from flask import Blueprint, jsonify, request
 from flask_jwt_extended import get_jwt_identity, jwt_required, create_access_token
-from flask_login import login_user, logout_user, current_user
+from flask_login import logout_user, current_user
 from flaskapp import db, bcrypt
-from flaskapp.forms import AdminRegistrationForm
 from flaskapp.models import User, Role
 from werkzeug.exceptions import HTTPException
 import googlemaps
@@ -14,11 +12,11 @@ from .restaurants import get_filters
 from dotenv import load_dotenv
 
 routes = Blueprint('routes', __name__)
+load_dotenv()
+gmaps = googlemaps.Client(key=os.getenv("REACT_APP_GOOGLE_MAPS_API_KEY"))
 
-# Custom Error Handlers
 @routes.errorhandler(HTTPException)
 def handle_http_exception(e):
-    """Return JSON instead of HTML for HTTP errors."""
     return jsonify(error=str(e), description=e.description), e.code
 
 @routes.errorhandler(404)
@@ -29,31 +27,20 @@ def handle_404_error(e):
 def handle_500_error(e):
     return jsonify(error='Server error', description='An internal server error occurred'), 500
 
-# Admin Registration
-@routes.route("/admin-register", methods=['GET', 'POST'])
-def register_admin():
-    form = AdminRegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        user_role = Role.query.filter_by(name="Admin").first()
-        user.role.append(user_role)
-        db.session.add(user)
-        db.session.commit()
-        flash('Account created for {} successfully, You can now login'.format(form.username.data), 'success')
-        return redirect(url_for('routes.login'))
-    return render_template('admin-register.html', title='Admin-Register', form=form)
+
+
 
 def is_valid_email(email):
-    """Validate the email format."""
     email_regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
     return re.match(email_regex, email, re.IGNORECASE)
 
 def is_strong_password(password):
-    """Check if the password is strong."""
     return len(password) >= 8 and re.search(r"[a-zA-Z]", password) and re.search(r"\d", password)
 
-@routes.route('/api/login', methods=['POST'])
+
+
+
+@routes.route('/api/signin', methods=['POST'])
 def login():
     data = request.get_json()
     if not data or 'username' not in data or 'password' not in data:
@@ -65,6 +52,7 @@ def login():
 
     access_token = create_access_token(identity=user.id)
     return jsonify({'user': {'username': user.username, 'email': user.email}, 'access_token': access_token})
+
 
 @routes.route("/api/register", methods=['POST'])
 def register_form():
@@ -94,7 +82,6 @@ def register_form():
     return jsonify({'message': 'Registration successful'}), 201
 
 
-# User Logout
 @routes.route("/api/logout")
 def logout_function():
     if not current_user.is_authenticated:
@@ -103,7 +90,7 @@ def logout_function():
     logout_user()
     return jsonify({'message': 'Logged out successfully'}), 200
 
-# Get User Information
+
 @routes.route("/user", methods=['GET'])
 @jwt_required()
 def get_user():
@@ -113,11 +100,10 @@ def get_user():
         return jsonify({'username': user.username, 'email': user.email})
     return jsonify({'message': 'User not found'}), 404
 
-@routes.route('/api/resturants', methods=["POST", "GET"])
+
+@routes.route('/api/restaurants', methods=["POST", "GET"])
 def resturants():
     if request.method == "POST":
-        load_dotenv()
-        gmaps = googlemaps.Client(key=os.getenv("REACT_APP_GOOGLE_MAPS_API_KEY"))
         data = request.get_json()
         preferences = data.get("input", {})
         neighborhood = preferences["location"]["neighborhood"]
@@ -125,7 +111,6 @@ def resturants():
             place_name = neighborhood + ", Manhattan, NY"
             geocode_result = gmaps.geocode(place_name)
             if geocode_result:
-                # Extract latitude and longitude
                 latitude = geocode_result[0]['geometry']['location']['lat']
                 longitude = geocode_result[0]['geometry']['location']['lng']
             else:
