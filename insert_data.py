ab13f9e5ee8ad7107c7f67d0dbb9e2b7d430e5dc
musaddique333 <mohammedmusaddiquemogal@gmail.com>
2024-07-23 01:24:05 +0100

    updted iinsertdata

--- insert_data.py
+++ insert_data.py
@@ -11,56 +11,56 @@ load_dotenv()
 
 Base = declarative_base()
 
-# class Restaurant(Base):
-#     __tablename__ = 'restaurant'
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
 
-#     restaurant_id = Column(String(22), primary_key=True)
-#     name = Column(String(255))
-#     url = Column(Text)
-#     image_url = Column(Text)
-#     rating = Column(Float)
-#     latitude = Column(Float, nullable=False)
-#     longitude = Column(Float, nullable=False)
-#     price = Column(String(10))
-#     display_address = Column(Text)
-#     zone_id = Column(Integer)
+    restaurant_id = Column(String(22), primary_key=True)
+    name = Column(String(255))
+    url = Column(Text)
+    image_url = Column(Text)
+    rating = Column(Float)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+    price = Column(String(10))
+    display_address = Column(Text)
+    zone_id = Column(Integer)
 
-# class Tags(Base):
-#     __tablename__ = 'tags'
+class Tags(Base):
+    __tablename__ = 'tags'
 
-#     restaurant_id = Column(String(22), primary_key=True)
-#     tags = Column(Text)
+    restaurant_id = Column(String(22), primary_key=True)
+    tags = Column(Text)
 
-# class Zones(Base):
-#     __tablename__ = 'zones'
+class Zones(Base):
+    __tablename__ = 'zones'
 
-#     zone_id = Column(Integer, primary_key=True)
-#     the_geom = Column(LONGTEXT, nullable=False)
-#     zone_name = Column(String(255))
-#     borough = Column(String(255))
+    zone_id = Column(Integer, primary_key=True)
+    the_geom = Column(LONGTEXT, nullable=False)
+    zone_name = Column(String(255))
+    borough = Column(String(255))
 
-# class Busyness(Base):
-#     __tablename__ = 'busyness'
+class Busyness(Base):
+    __tablename__ = 'busyness'
 
-#     restaurant_id = Column(String(22), primary_key=True)
-#     Monday_populartimes = Column(String(255))
-#     Tuesday_populartimes = Column(String(255))
-#     Wednesday_populartimes = Column(String(255))
-#     Thursday_populartimes = Column(String(255))
-#     Friday_populartimes = Column(String(255))
-#     Saturday_populartimes = Column(String(255))
-#     Sunday_populartimes = Column(String(255))
+    restaurant_id = Column(String(22), primary_key=True)
+    Monday_populartimes = Column(String(255))
+    Tuesday_populartimes = Column(String(255))
+    Wednesday_populartimes = Column(String(255))
+    Thursday_populartimes = Column(String(255))
+    Friday_populartimes = Column(String(255))
+    Saturday_populartimes = Column(String(255))
+    Sunday_populartimes = Column(String(255))
 
-class User(Base):
-    __tablename__ = 'user'
+# class User(Base):
+#     __tablename__ = 'user'
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String(80), unique=True, nullable=False)
-    email = Column(String(120), unique=True, nullable=False)
-    image_file = Column(String(20), nullable=False, default='default.jpg')
-    password = Column(String(60), nullable=False)
-    is_confirmed = Column(Boolean, nullable=False, default=False)
-    confirmed_on = Column(DateTime, nullable=True)
+#     id = Column(Integer, primary_key=True)
+#     username = Column(String(80), unique=True, nullable=False)
+#     email = Column(String(120), unique=True, nullable=False)
+#     image_file = Column(String(20), nullable=False, default='default.jpg')
+#     password = Column(String(60), nullable=False)
+#     is_confirmed = Column(Boolean, nullable=False, default=False)
+#     confirmed_on = Column(DateTime, nullable=True)
 
 
 def load_data_into_table(file_path, model):
@@ -77,11 +77,11 @@ def load_data_into_table(file_path, model):
         session.close()
 
 csv_files = {
-    # 'restaurants': 'table_data/restaurant.csv',
-    # 'tags': 'table_data/tags.csv',
-    # 'zones': 'table_data/zones.csv',
-    # 'restaurant_busyness': 'table_data/busyness.csv',
-    'users': 'table_data/user.csv',
+    'restaurants': 'table_data/restaurant.csv',
+    'tags': 'table_data/tags.csv',
+    'zones': 'table_data/zones.csv',
+    'restaurant_busyness': 'table_data/busyness.csv',
+    # 'users': 'table_data/user.csv',
 }
 
 DEV_DATABASE_URI = os.getenv("DEV_DATABASE_URI")
@@ -91,11 +91,11 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# load_data_into_table(csv_files['restaurants'], "restaurant")
-# load_data_into_table(csv_files['tags'], "tags")
-# load_data_into_table(csv_files['zones'], "zones")
-# load_data_into_table(csv_files['restaurant_busyness'], "busyness")
-load_data_into_table(csv_files['users'], "user")
+load_data_into_table(csv_files['restaurants'], "restaurant")
+load_data_into_table(csv_files['tags'], "tags")
+load_data_into_table(csv_files['zones'], "zones")
+load_data_into_table(csv_files['restaurant_busyness'], "busyness")
+# load_data_into_table(csv_files['users'], "user")
 
 
 print("Data loaded successfully.")
